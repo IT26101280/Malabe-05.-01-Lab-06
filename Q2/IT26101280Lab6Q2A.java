b61f81eab51f19d76923915933d7e55312e362a9
IT26101280 <it26101280@my.sliit.lk>
2026-09-15 11:44:19 +0530

    IT26101280Lab6Q2.java

---
+++ Q2/IT26101280Lab6Q2A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class IT26101280Lab6Q2A{
+	public static void main(String[] args) {
+		int counter = 1;
+		
+		while (counter <= 10){
+			System.out.print(counter + " ");
+			
+			counter++;
+		}System.out.println();
+	}
+}
